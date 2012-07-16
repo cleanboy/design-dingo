e964132e0f030f86002f3041c1bc97cc138ac7c0
Matt <matthew.jonat@gmail.com>
2012-07-16 17:04:56 +0100

    added footer and some additional text, as well as enlarge image on hover

--- javascript/dingo.js
+++ javascript/dingo.js
@@ -5,29 +5,47 @@
 
 $("document").ready(function(){
 	
+	$('.sitePreview img, .sitePreview').hover(
+		function(){
+		$(this).animate({
+			'top':"-=10",
+			'left':"-=10",
+			"width":"300px",
+			"height":"200px",
+			}, 600);
+		},
+		function(){
+			$(this).animate({
+			"width":"280px",
+			"height":"180px",
+			'top':"+=10",
+			'left':"+=10",
+			}, 600);
+		}
+	);
+	
 	$('.sitePreview').toggle(
-		//site descrition drop box motion up
-		
 		function (){
-			
+		//site descrition drop box motion up
 			$(this).next().animate({
-				"width":"290px",
+				"width":"280px",
 				"height":"200px",
 				'left':"0",
 				'top': '+=190'
 			}, 600);
-			
 		},
 		// and down
 		function (){
 			$(this).next().animate({
-				"width":"270px",
-				"height":"170px",
-				'left':"10",
+				"width":"260px",
+				"height":"160px",
+				'left':"10px",
 				'top': '-=190'
 			}, 600);
 			
 		});
+		
+	
 });
 
 
