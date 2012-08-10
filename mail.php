ef9b6737b443af8cb095d73e5fb4f4ab3300c689
Matt <matthew.jonat@gmail.com>
2012-08-10 13:50:14 +0100

    uploaded and tested, and fixed mail form online

--- mail.php
+++ mail.php
@@ -5,8 +5,9 @@ $message = $_POST['message'];
 $formcontent = "From: $name \n Message: $message";
 $recipient = "matthew.jonat@gmail.com";
 $subject = "Mind Equals Blown Mail";
+header("Location: index.html");
 $mailheader = "From: $email \r\n";
 
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error");
-echo"Thank You";
+
     
